6d36fdf0d8a2748bb9910694362d59a384827584
toddeklof <toddeklof@hotmail.com>
2020-11-20 14:58:41 -0800

    SaloonDoorAnimationsAndScript

---
+++ Assets/Scripts/ToggleTeleportAnchorTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleTeleportAnchorTarget : MonoBehaviour
+{
+    public Transform orginalPosition;
+    public Transform secondPosition;
+
+    private bool isOnOriginalPosition = true;
+
+    
+    public void OnTeleportTrigger()
+    {
+        Transform targetTransform = isOnOriginalPosition ? secondPosition : orginalPosition;
+        transform.position = targetTransform.position;
+        transform.rotation = targetTransform.rotation;
+
+        isOnOriginalPosition = !isOnOriginalPosition;
+    } 
+   
+}
